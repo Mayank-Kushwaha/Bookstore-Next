75eb3f3efa48496041f851d395579a6392ab7152
LordMashh <mayankkush0842@gmail.com>
2023-09-20 17:23:31 +0530

    frontend update

--- app/Search/page.jsx
+++ app/Search/page.jsx
@@ -1,9 +1,12 @@
+import {BiSearch} from "react-icons/bi"
+
 export default function Search() {
     return (
-        <div className="max-w-6xl w-full mx-auto px-4 py-6 justify-start md:px-8">
-             <div className="font-main place-items-center text-xl mr-auto md:text-2xl my-1 block w-full md:pr-10 rounded border-2 border-gray-300 bg-primary py-1 px-2 font-normal outline-skin-accent ">
+        <div className="max-w-6xl w-full mx-auto px-4 py-6 justify-start md:px-8 ">
+             <div className="font-MyFont flex divide-x justify-between items-center text-xl mr-auto md:text-2xl my-1  w-full md:pr-10 rounded border-2 border-gray-300 bg-primary py-1 px-2 font-normal outline-skin-accent ">
         {" "}
       Search Here
+      <BiSearch/>
       </div>
         </div>
     );

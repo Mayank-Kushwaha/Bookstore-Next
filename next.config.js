d30313be3bf3ebcc6d39c5050d01f71e884ec0e2
LordMashh <mayankkush0842@gmail.com>
2023-09-22 15:39:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {    
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: 'books.google.com',
-
-          },
-        ],
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'books.google.com',
       },
-    env: {
-      BASE_URL: process.env.BASE_URL,
-    }};
+    ],
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+  webpack: function(config) {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

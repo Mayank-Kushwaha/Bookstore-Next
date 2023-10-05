098f4c9a52d88d3989378b2383871141c5c6a2f8
LordMashh <mayankkush0842@gmail.com>
2023-10-05 17:02:42 +0530

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import toast from "react-hot-toast";

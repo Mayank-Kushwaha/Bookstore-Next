84807b82d770bd202388b175dfbc9143e5b438df
LordMashh <mayankkush0842@gmail.com>
2023-10-08 20:26:05 +0530

    commit 1

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,7 +3,7 @@ import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-
+import { process } from 'process';
 
 
 export const authOptions = {

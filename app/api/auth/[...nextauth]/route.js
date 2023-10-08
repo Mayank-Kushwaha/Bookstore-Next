0d3125b87349fd908e1b976c7a0956330bad6bc6
LordMashh <mayankkush0842@gmail.com>
2023-10-08 19:50:38 +0530

    final commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,6 +4,7 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 require("dotenv").config();
+import { process } from 'process';
 
 const { NEXTAUTH_SECRET } = process.env;
 // const { NEXT_PUBLIC_DOMAIN } = process.env;

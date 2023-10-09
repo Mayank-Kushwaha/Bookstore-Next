344a9dece60fb8de090e963b0d402d9d5e2157fd
LordMashh <mayankkush0842@gmail.com>
2023-10-09 21:13:10 +0530

    final commit contactus

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { IoMailUnreadOutline } from "react-icons/io5";
@@ -5,8 +6,22 @@ import { PiTelegramLogo } from "react-icons/pi";
 import { SlSocialInstagram } from "react-icons/sl";
 import { FiFacebook } from "react-icons/fi";
 import { RiSendPlaneLine } from "react-icons/ri";
+import { useForm } from '@formspree/react';
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
 
 export default function Contact() {
+  const router = useRouter();
+
+  const [state, handleSubmit] = useForm("mqkvgrjn");
+
+  if (state.succeeded) {
+   
+    toast.success("Message sent successfully");
+    router.push("/")
+  }
+
   return (
     <div className="max-w-6xl w-full mx-auto px-4 py-6 justify-start md:px-8">
       <h1 className="font-main text-xl my-4 font-semibold mr-auto md:text-2xl ">
@@ -37,14 +52,17 @@ export default function Contact() {
         </div>
         <div className="flex-1 pt-8 md:pt-0 md:pl-10 xl:pl-20">
           <h2 className="text-xl mb-2 font-MyFont font-bold">Any Query!</h2>
+          <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="font-MyFont font-medium">
               Email Address
               <input
                 placeholder="Enter Your Valid Email"
                 className="my-1 block w-full md:pr-10 rounded border-2 border-gray-300 bg-primary py-1 px-2 font-normal outline-skin-accent"
+                id="email"
                 type="email"
                 name="email"
+                
               />
             </label>
           </div>
@@ -54,8 +72,10 @@ export default function Contact() {
               <input
                 placeholder="Enter Subject of your Query"
                 className="my-1 block w-full md:pr-10 rounded border-2 border-gray-300 bg-primary py-1 px-2 font-normal outline-skin-accent"
+                id="subject"
                 type="text"
-                name="username"
+                name="subject"
+               
               />
             </label>
           </div>
@@ -63,21 +83,25 @@ export default function Contact() {
             <label className="font-MyFont font-medium">
               Query
               <textarea
-                placeholder="Hey, I just want to contact you"
+                placeholder="Hey, I just want to connect!!"
                 rows="4"
                 className="y-1 block w-full rounded border-2 border-skin-gray bg-skin-base py-1 px-2
                   font-normal outline-skin-accent"
-                name="address"
+                  id="query"
+                  name="query"
+            
               ></textarea>
             </label>
           </div>
           <button
-            type="button"
+            type="submit"
+            disabled={state.submitting}
             className="bg-textgray text-white w-full flex justify-center py-2 px-2 mt-2 font-MyFont text-lg font-medium md:rounded md:py-1"
           >
             <RiSendPlaneLine className="mt-1 text-white mr-3" />
             <span>Send</span>
           </button>
+          </form>
         </div>
       </div>
     </div>

620974e83bb0fa1455217420d99170ac16f7a602
LordMashh <mayankkush0842@gmail.com>
2023-10-23 22:50:51 +0530

    update cookie

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,36 +1,53 @@
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
-import { cookies } from 'next/headers'
+import { cookies } from "next/headers";
 
 connectMongoDB();
 
 export async function POST(req) {
+  const cookie = cookies();
   const { email, password } = await req.json();
   if (!email || !password) {
-    return NextResponse.json({ error: "Please provide all the fields" }, { status: 422 });
+    return NextResponse.json(
+      { error: "Please provide all the fields" },
+      { status: 422 }
+    );
   }
 
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return NextResponse.json({ error: "User doesn't exist with that email" }, { status: 404 });
+      return NextResponse.json(
+        { error: "User doesn't exist with that email" },
+        { status: 404 }
+      );
     }
 
     const doMatch = await bcrypt.compare(password, user.password);
     if (doMatch) {
       const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "7d"
+        expiresIn: "7d",
       });
-      const oneDay = 7 * 24 * 60 * 60 * 1000
-      cookies().set( token, { expires: Date.now() - oneDay })
+     
+      cookie.set("token", token, {
+       
+        secure: true,
+        maxAge:  7 * 24 * 60 * 60, // 7 days
+      });
       const { name, email } = user;
 
-      return NextResponse.json({ token, user: { name, email } }, { status: 201 });
+      return NextResponse.json(
+        { token, user: { name, email } },
+        { status: 201 }
+      );
     } else {
-      return NextResponse.json({ error: "Email or password don't match" }, { status: 401 });
+      return NextResponse.json(
+        { error: "Email or password don't match" },
+        { status: 401 }
+      );
     }
   } catch (error) {
     return NextResponse.json(
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {    
   experimental: {
     appDir: true,
+    serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
